c8875ed420b8148987d878e2dc978b9a09601ab6
Markuss95 <51216200+Markuss95@users.noreply.github.com>
2024-10-14 14:53:29 +0200

    Ignore warning

--- index.js
+++ index.js
@@ -5,5 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {LogBox} from 'react-native';
+
+LogBox.ignoreLogs([
+  'Warning: TextElement: Support for defaultProps will be removed from function components in a future major release. Use JavaScript default parameters instead.',
+]);
 
 AppRegistry.registerComponent(appName, () => App);
