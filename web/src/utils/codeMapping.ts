044541e7f24497b343ffbc25a3d34106dbf79483
Marko Krajina <mkrajina@koteks.local>
2025-11-12 09:41:46 +0100

    .

--- web/src/utils/codeMapping.ts
+++ web/src/utils/codeMapping.ts
@@ -71,7 +71,7 @@ export const codeToTextMapping: Record<string, string> = {
   '25095': 'Livana',
   '25097': 'Darda - Izra. Man. Pov.',
   '25099': 'Farma Belje Popovac',
-  '25100': 'Eurospin Retfala',
+  '25100': 'Tranzit Eurospin Retfala',
   '25104': 'Belišće igralište',
   '5014': 'Betonara',
 };
